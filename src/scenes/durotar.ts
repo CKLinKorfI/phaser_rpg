46a5eed268ac3d45b7b7d3bad6978164d48be864
Михаил Бадерко <badmeha05@gmail.com>
2026-01-29 23:28:09 +0500

    Занятие №2

--- src/scenes/durotar.ts
+++ src/scenes/durotar.ts
@@ -1,1 +1,21 @@
-export class Durotar {}
+import durotarJSON from '../assets/durotar.json';
+import {LAYERS, SIZES, TILES} from '../utils/constants.ts';
+
+export class Durotar extends Phaser.Scene {
+    constructor () {
+        super('DurotarScene');
+    }
+
+    preload(){
+        this.load.image('durotar', 'src/assets/durotar.png');
+        this.load.tilemapTiledJSON('map', 'src/assets/durotar.json')
+    }
+
+    create() {
+        const map = this.make.tilemap({key: 'map'});
+        const tileSet = map.addTilesetImage(durotarJSON.tilesets[0].name, TILES.DUROTAR, SIZES.TILE, SIZES.TILE);
+        if (!tileSet) return;
+        const groundLayer = map.createLayer(LAYERS.GROUND, tileSet, 0, 0);
+        const wallsLayer = map.createLayer(LAYERS.WALLS, tileSet, 0, 0);
+    }
+}
