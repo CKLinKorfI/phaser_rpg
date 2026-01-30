02b44851c31d20d9d17bcd0e2d55ba9097d512bb
Михаил Бадерко <badmeha05@gmail.com>
2026-01-30 06:34:58 +0500

    Занятие №3

--- src/scenes/durotar.ts
+++ src/scenes/durotar.ts
@@ -1,14 +1,21 @@
 import durotarJSON from '../assets/durotar.json';
-import {LAYERS, SIZES, TILES} from '../utils/constants.ts';
+import { Player } from '../entities/player.ts';
+import {LAYERS, SIZES, SPEITES, TILES} from '../utils/constants.ts';
 
 export class Durotar extends Phaser.Scene {
+    private player?: Player;
+
     constructor () {
         super('DurotarScene');
     }
 
     preload(){
-        this.load.image('durotar', 'src/assets/durotar.png');
-        this.load.tilemapTiledJSON('map', 'src/assets/durotar.json')
+        this.load.image(TILES.DUROTAR, 'src/assets/durotar.png');
+        this.load.tilemapTiledJSON('map', 'src/assets/durotar.json');
+        this.load.spritesheet(SPEITES.PLAYER,'src/assets/characters/alliance.png', {
+            frameWidth: SIZES.PLAYER.WIDTH,
+            frameHeight: SIZES.PLAYER.HEIGHT
+        } );
     }
 
     create() {
@@ -17,5 +24,11 @@ export class Durotar extends Phaser.Scene {
         if (!tileSet) return;
         const groundLayer = map.createLayer(LAYERS.GROUND, tileSet, 0, 0);
         const wallsLayer = map.createLayer(LAYERS.WALLS, tileSet, 0, 0);
+
+        this.player = new Player(this, 400, 250, SPEITES.PLAYER)
+    }
+
+    update(time: number, delta: number): void {
+        this.player?.update(delta);
     }
 }
